2d08416b68fd94c9fb0224fc4c8df52599953588
Antoine Frau <antoine.frau1108@gmail.com>
2019-11-13 12:14:56 -0500

    :tada: Initial commit - Timer start

---
+++ FootResultat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FootResultat
+//
+//  Created by Antoine Frau on 13/11/2019.
+//  Copyright © 2019 Antoine Frau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
